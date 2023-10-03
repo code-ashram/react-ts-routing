7e5c05d0c539bb8ef52f315add2ff69073458a16
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-10-03 21:56:54 +0300

    add client

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+
+import { Comment, Post } from './api/models.ts'
+import { getComments, getPosts } from './api'
+
+import './App.css'
+
+const App = () => {
+  // const [posts, setPosts] = useState<Post[]>()
+  const [comments, setComments] = useState<Comment[]>()
+
+  useEffect(() => {
+    const controller = new AbortController()
+    const { signal } = controller
+
+    // getPosts(signal).then((response) => setPosts(response))
+    getComments(signal, 1).then((response) => setComments(response))
+
+    return () => {
+      controller.abort()
+    }
+  }, [])
+
+  return (
+    <div>
+      <header>
+        <nav>
+          <a href="#">Home</a>
+        </nav>
+      </header>
+
+      <main>
+        <section>
+          <ul>
+            {/* {posts?.map((post) => { */}
+            {/*   return <li key={post.id}>{post.title}</li> */}
+            {/* })} */}
+            {comments?.map((comment) => {
+              return <li key={comment.id}>{comment.name}</li>
+            })}
+          </ul>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default App
