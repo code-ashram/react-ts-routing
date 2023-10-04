5c954c73198ed2015a2f0b0845063faf82c2e9d0
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-10-04 16:50:20 +0300

    prepare some styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ import { getComments, getPosts } from './api'
 import './App.css'
 
 const App = () => {
-  // const [posts, setPosts] = useState<Post[]>()
-  const [comments, setComments] = useState<Comment[]>()
+  const [posts, setPosts] = useState<Post[]>()
+  // const [comments, setComments] = useState<Comment[]>()
 
   useEffect(() => {
     const controller = new AbortController()
     const { signal } = controller
 
-    // getPosts(signal).then((response) => setPosts(response))
-    getComments(signal, 1).then((response) => setComments(response))
+    getPosts(signal).then((response) => setPosts(response))
+    // getComments(signal, 1).then((response) => setComments(response))
 
     return () => {
       controller.abort()
@@ -31,14 +31,14 @@ const App = () => {
 
       <main>
         <section>
-          <ul>
-            {/* {posts?.map((post) => { */}
-            {/*   return <li key={post.id}>{post.title}</li> */}
-            {/* })} */}
-            {comments?.map((comment) => {
-              return <li key={comment.id}>{comment.name}</li>
+          <ol>
+            {posts?.map((post) => {
+              return <li key={post.id}>{post.title}</li>
             })}
-          </ul>
+            {/* {comments?.map((comment) => { */}
+            {/*   return <li key={comment.id}>{comment.name}</li> */}
+            {/* })} */}
+          </ol>
         </section>
       </main>
     </div>
